dec63ca6752bbbde50ea57281e842a05eb6bf469
santi7870 <santiago.panchi@udla.edu.ec>
2024-10-30 08:49:32 -0500

    Commit2

--- PANCHI_PROYECTO1/Models/Celular.cs
+++ PANCHI_PROYECTO1/Models/Celular.cs
@@ -1,16 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PANCHI_PROYECTO1.Models
 {
     public class Celular
     {
         [Key]
-        public int Id { get; set; }
+        public int IdCelular { get; set; }
+
+        [Required(ErrorMessage ="El campo de modelo es obligatorio")]
+        [MaxLength(50)]
+
+        
 
         public string Modelo { get; set; }
 
+        [ForeignKey(nameof(Celular))]
+
+
+        [Range(2000,2025)]
         public int Anio { get; set; }
+
+        [Range(1000,10000)]
         public int Precio { get; set; }
+
+        [ForeignKey("SPanchi")]
+        public int SPanchiId { get; set; }
+
+        public SPanchi sPanchi { get; set; }
+
 
 
 
--- PANCHI_PROYECTO1/Models/SPanchi.cs
+++ PANCHI_PROYECTO1/Models/SPanchi.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PANCHI_PROYECTO1.Models
 {
@@ -6,17 +8,25 @@ namespace PANCHI_PROYECTO1.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage ="El campo de nombre es obligatorio")]
 
-        [MaxLength(100)]
+        [MaxLength(100, ErrorMessage = "No puede exeder de 100 caracteres")]
         public string Nombre { get; set; }
-
-        []
+        [Range(0.2, 500)]
+     
 
         public float Peso { get; set; }
 
+        [Required]
         public Boolean Extranjero { get; set; }
 
+        [DataType(DataType.Date)]
+        [Display(Name="Fecha de registro")]
+
         public  string Fecha { get; set; }
+
+        public Celular celular { get; set; }
+
 
 
 
