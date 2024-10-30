019cfc0939ffb1f31be15afdf2c99144bcf42a2f
santi7870 <santiago.panchi@udla.edu.ec>
2024-10-30 08:30:15 -0500

    Commit1

---
+++ PANCHI_PROYECTO1/Models/Celular.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PANCHI_PROYECTO1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int Anio { get; set; }
+        public int Precio { get; set; }
+
+
+
+
+
+    }
+}
---
+++ PANCHI_PROYECTO1/Models/SPanchi.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PANCHI_PROYECTO1.Models
+{
+    public class SPanchi
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+
+        []
+
+        public float Peso { get; set; }
+
+        public Boolean Extranjero { get; set; }
+
+        public  string Fecha { get; set; }
+
+
+
+
+
+       
+
+
+         
+
+
+    }
+}
