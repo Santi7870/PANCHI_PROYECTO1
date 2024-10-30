16fc3ab7bdf253a1efb8f3166b31c3520a3e3de4
Santi7870 <santiago.panchi@udla.edu.ec>
2024-10-30 14:18:16 -0500

    commit7

--- PANCHI_PROYECTO1/wwwroot/js/site.js
+++ PANCHI_PROYECTO1/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function CrearAlerta() {
+
+    alert("Mi nombre es: Santi Panchi y mi hobbie es: Ver al Real Madrid");
+
+
+}
+
